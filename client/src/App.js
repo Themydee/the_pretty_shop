0e34e8fd69418ac9b11d16ce61a74acd87672231
Akanbi Bello Temidayo <nifetemiboy@gmail.com>
2024-01-12 22:28:31 +0100

    routers completed

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,10 @@ import {
   Route
 } from 'react-router-dom'
 import Shop from './pages/Shop'
+import ShopCategory from './pages/ShopCategory'
+import Product from './pages/Product'
+import Cart from './pages/Cart'
+import Register from './pages/Register'
 
 function App () {
   return (
@@ -14,6 +18,15 @@ function App () {
       <Navbar />
       <Routes>
         <Route path='/' element={<Shop />} />
+        <Route path='/men' element={<ShopCategory category='men'/>} />
+        <Route path='/women' element={<ShopCategory category='women'/>} />
+        <Route path='/kids' element={<ShopCategory category='kids' />} />
+        <Route path='/product' element={<Product />}>
+          <Route path=':productId' element={<Product />}/>
+        </Route>
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/login' element={<Register />} />
+
       </Routes>
       </BrowserRouter>
      
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import './navbar.css'
 
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
+import { Link } from 'react-router-dom/dist/umd/react-router-dom.development'
 
 const Navbar = () => {
     const [active, setActive] = useState("shop")
@@ -14,15 +15,21 @@ const Navbar = () => {
         </div>
 
         <ul className="nav-menu">
-            <li onClick={() => {setActive("shop")}}>Shop {active === "shop" ? <hr />: <></>}</li>
-            <li onClick={() => {setActive("men")}}>Men{active === "men" ? <hr />: <></>}</li>
-            <li onClick={() => {setActive("women")}}>Women{active === "women" ? <hr />: <></>}</li>
-            <li onClick={() => {setActive("kids")}}>Kids{active === "kids" ? <hr />: <></>}</li>
+            <li onClick={() => {setActive("shop")}}><Link style={{ color: 'black', textDecoration: 'none'}} to='/'>Shop</Link>{active === "shop" ? <hr />: <></>}</li>
+            <li onClick={() => {setActive("men")}}><Link style={{ color: 'black', textDecoration: 'none'}}to='/men'>Men</Link>{active === "men" ? <hr />: <></>}</li>
+            <li onClick={() => {setActive("women")}}><Link style={{ color: 'black', textDecoration: 'none'}} to='/women'>Women</Link>{active === "women" ? <hr />: <></>}</li>
+            <li onClick={() => {setActive("kids")}}><Link style={{ color: 'black', textDecoration: 'none'}} to='/kids'>Kids</Link>{active === "kids" ? <hr />: <></>}</li>
         </ul>
 
         <div className="nav-login-cart">
-            <button>Login</button>
-            <img src={cart_icon} alt="" />
+            <Link to='/login'> 
+                <button>Login</button>
+            </Link>
+           
+           <Link to='/cart'>
+                <img src={cart_icon} alt="" />
+           </Link>
+            
             <div className="nav-cart-count">0</div>
         </div>
     </div>
