23539eccc8856ab5dab70cc191d40d1cc49cedd2
Akanbi Bello Temidayo <nifetemiboy@gmail.com>
2024-01-28 02:42:37 +0100

    media queries navbar and backend installation

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './hero.css'
 import hand_icon from  "../Assets/hand_icon.png"
-import arrow_icon from  "../Assets/arrow.png"
+// import arrow_icon from  "../Assets/arrow.png"
 import hero_image from "../Assets/hero_image.png"
 
 const Hero = () => {
@@ -9,19 +9,20 @@ const Hero = () => {
     <div className='hero'>
         <div className="hero-left">
             <h2>NEW ARRIVALS ONLY</h2>
+            <hr />
 
             <div className="hand-hand-icon">
-                <p>new  <img style={{ width: '40px', }}src={hand_icon} alt="" /></p>
+                <p>New <img style={{ width: '40px', }}src={hand_icon} alt="" /></p>
             </div>
 
             <p>Collection</p>
-            <p>for everyone</p>
+            <p>For Everyone</p>
         </div>
 
-        <div className="hero-latest-btn">
+        {/* <div className="hero-latest-btn">
             <div>Latest Collection</div>
             <img src={arrow_icon} alt="" />
-        </div>
+        </div> */}
 
         <div className="hero-right">
             <img style={{width: '400px'}}src={hero_image} alt="" />
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,22 +1,33 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useRef, useState } from 'react'
 import './navbar.css'
 
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 import { Link } from 'react-router-dom/dist/umd/react-router-dom.development'
 import { ShopContext } from '../../context/ShopContext'
+import nav_drop_down from '../Assets/nav-drop.jpg'
 
 const Navbar = () => {
     const [active, setActive] = useState("shop")
     const {getTotalCartItems} = useContext(ShopContext);
+
+    const menuRef = useRef();
+
+    const dropdown_toggle = (e) => {
+        menuRef.current.classList.toggle('nav-menu-visible');
+        e.target.classList.toggle('open');
+    }
   return (
     <div className='navbar'>
         <div className="nav-logo">
             <img src={logo}alt="" />
-            <p>The Pretty Shop</p>
+            <p>OYIZAS SHOP</p>
         </div>
 
-        <ul className="nav-menu">
+        <img className='nav-dropdown' onClick={dropdown_toggle} src={nav_drop_down} style={{width: '30px' }}alt="" />
+
+        
+        <ul ref={menuRef} className="nav-menu">
             <li onClick={() => {setActive("shop")}}><Link style={{ color: 'black', textDecoration: 'none'}} to='/'>Shop</Link>{active === "shop" ? <hr />: <></>}</li>
             <li onClick={() => {setActive("men")}}><Link style={{ color: 'black', textDecoration: 'none'}}to='/men'>Men</Link>{active === "men" ? <hr />: <></>}</li>
             <li onClick={() => {setActive("women")}}><Link style={{ color: 'black', textDecoration: 'none'}} to='/women'>Women</Link>{active === "women" ? <hr />: <></>}</li>
